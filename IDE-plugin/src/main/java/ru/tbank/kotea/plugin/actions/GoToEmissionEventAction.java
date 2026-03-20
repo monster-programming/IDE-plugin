42130d8fdd068cb6a161a4a2349d3dbd2303c8d9
deademous <opolindmitry@gmail.com>
2026-03-20 21:50:25 +0300

    register plugin

--- IDE-plugin/src/main/java/ru/tbank/kotea/plugin/actions/GoToEmissionEventAction.java
+++ IDE-plugin/src/main/java/ru/tbank/kotea/plugin/actions/GoToEmissionEventAction.java
@@ -1,5 +1,19 @@
 package ru.tbank.kotea.plugin.actions;
 
-public class GoToEmissionEventAction {
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
 
+public class GoToEmissionEventAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        Project project = anActionEvent.getProject();
+        Messages.showInfoMessage(project, "Тест налиичя кнопки", "KoTEA Plugin");
+    }
 }
